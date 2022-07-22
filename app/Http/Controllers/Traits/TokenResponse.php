5c9a248338f40c1654db86412560898b847c50a5
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-07-22 22:39:12 +0200

    Remove dead code

--- app/Http/Controllers/Traits/TokenResponse.php
+++ app/Http/Controllers/Traits/TokenResponse.php
@@ -6,18 +6,8 @@ trait TokenResponse
 {
     protected function respondWithToken($token): \Illuminate\Http\JsonResponse
     {
-//        return response()->json([
-////            'access_token' => $token,
-//            'token' => $token,
-//            'token_type' => 'bearer',
-//            'expires_in' => auth()->factory()->getTTL() * 60
-//        ])->withHeaders([
-//            'Authorization' => 'Bearer '.$token,
-//        ]);
-
         return response()->json([
             'access_token' => $token,
-//            'token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
         ]);
