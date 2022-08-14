c82fb70d293dc5ae02f8449644d6db0ec8ebaca4
Richard Trujillo <richard.trujillo.torres@gmail.com>
2022-08-14 16:02:18 +0200

    Add demo seeder

---
+++ database/seeders/DemoSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DemoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'demo',
+            'email' => 'demo@chess-repository-service.io',
+            'password' => Hash::make('secret'),
+        ]);
+    }
+}
